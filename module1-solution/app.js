1bea70cca21761bd335f50b99bfddb0b449e44c5
Beswick <wayne.beswick@unisys.com>
2017-09-15 08:56:10 +0100

    Final version

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,12 +8,30 @@ angular.module('LunchCheck', [])
 LunchCheckController.inject = ['$scope'];
 
 function LunchCheckController ($scope) {
-
-  $scope.checkMessage = "XXXX";
+  $scope.lunchEntry = "";
+  $scope.checkMessage = "";
 
   $scope.checkLunchEntry = function () {
-    $scope.checkMessage = "XXXX";
-    return "Hello Coursera!";
+    var lunchItems = $scope.lunchEntry.split(',');
+    var noOfItems = lunchItems.length;
+    var returnMsg = "";
+// remove entry items from the list
+    for (var i = 0; i < lunchItems.length; i++) {
+      if (lunchItems[i].trim() == "") {
+        noOfItems--
+      }
+    }
+
+    if ($scope.lunchEntry.trim() == "") {
+      returnMsg = "Please enter data first"
+    }
+    else if (noOfItems <= 3) {
+      returnMsg = "Enjoy!"
+    }
+    else {
+      returnMsg = "Too much!"
+    }
+    $scope.checkMessage = returnMsg;
   };
 };
 
